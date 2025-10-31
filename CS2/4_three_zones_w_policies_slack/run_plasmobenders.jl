9294e342edff9840c8979ee930dedb624ea4af3e
David Cole <dlcdavidc@gmail.com>
2025-10-31 12:00:53 -0400

    updated result files

--- CS2/4_three_zones_w_policies_slack/run_plasmobenders.jl
+++ CS2/4_three_zones_w_policies_slack/run_plasmobenders.jl
@@ -20,42 +20,23 @@ setup["settings_path"] = GenX.get_settings_path(case);
 inputs = GenX.load_inputs(setup, case);
 
 # Set subproblem solver
-solver = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => false, "Method" => 2, "Crossover"=> 0)
-
-#PLANNING_OPTIMIZER = GenX.configure_benders_planning_solver(setup["settings_path"]);
-#SUBPROB_OPTIMIZER =  GenX.configure_benders_subprob_solver(setup["settings_path"]);
-graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, solver)
-
-#graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,PLANNING_OPTIMIZER,SUBPROB_OPTIMIZER)
+solver = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => false, "Method" => 2, "Crossover"=> 0, "BarConvTol" => 1e-10)
 
 
-set_to_node_objectives(graph)
-set_optimizer(graph, Gurobi.Optimizer)
-#optimize!(graph)
+for i in 1:10
+    println("RUNNING ITERATION $i")
+    graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, solver)
+    # Note that this graph is not partitioned into subgraphs. The `BendersAlgorithm` constructore
+    # will do this automatically, placing each node on a subgraph, if the user does not define the 
+    # subgraphs themselves; this is the approach used for the regularized instance just to highlight
+    # both ways of doing the partitioning
 
-BendersAlg = BendersAlgorithm(graph, local_nodes(graph)[1],
-    regularize = false,
-    multicut = true,
-    parallelize_benders = true,
-    tol = 1e-4,
-    solver=solver,
-    max_iters = 350,
-    warm_start = false,
-)
-run_algorithm!(BendersAlg)
+    node_membership_vector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    partition = Plasmo.Partition(graph, node_membership_vector)
+    apply_partition!(graph, partition)
 
-df = DataFrame()
-df[!, "UB"] = BendersAlg.upper_bounds
-df[!, "LB"] = BendersAlg.lower_bounds
-df[!, "Time"] = BendersAlg.time_iterations
-CSV.write((@__DIR__)*"/benders_no_regularization.csv", df)
-
-
-graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, solver)
-
-@suppress_err begin
-    BendersAlg = BendersAlgorithm(graph, local_nodes(graph)[1],
-        regularize = true,
+    BendersAlg = BendersAlgorithm(graph, local_subgraphs(graph)[1],
+        regularize = false,
         multicut = true,
         parallelize_benders = true,
         tol = 1e-4,
@@ -69,5 +50,49 @@ graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, so
     df[!, "UB"] = BendersAlg.upper_bounds
     df[!, "LB"] = BendersAlg.lower_bounds
     df[!, "Time"] = BendersAlg.time_iterations
-    CSV.write((@__DIR__)*"/benders_regularization.csv", df)
+    CSV.write((@__DIR__)*"/results/benders_no_regularization_run$i.csv", df)
+
+    graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, solver)
+
+    @suppress_err begin
+        BendersAlg = BendersAlgorithm(graph, local_nodes(graph)[1],
+            regularize = true,
+            multicut = true,
+            parallelize_benders = true,
+            tol = 1e-4,
+            solver=solver,
+            max_iters = 350,
+            warm_start = false,
+        )
+        run_algorithm!(BendersAlg)
+
+        df = DataFrame()
+        df[!, "UB"] = BendersAlg.upper_bounds
+        df[!, "LB"] = BendersAlg.lower_bounds
+        df[!, "Time"] = BendersAlg.time_iterations
+        CSV.write((@__DIR__)*"/results/benders_regularization_run$i.csv", df)
+    end
+
+    graph,linking_variables_maps = GenX.generate_graph_model(setup,inputs,solver, solver)
+
+    node_membership_vector = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+    partition = Plasmo.Partition(graph, node_membership_vector)
+    apply_partition!(graph, partition)
+
+    BendersAlg = BendersAlgorithm(graph, local_subgraphs(graph)[1],
+        regularize = false,
+        multicut = true,
+        parallelize_benders = true,
+        tol = 1e-4,
+        solver=solver,
+        max_iters = 350,
+        warm_start = false,
+    )
+    run_algorithm!(BendersAlg)
+
+    df = DataFrame()
+    df[!, "UB"] = BendersAlg.upper_bounds
+    df[!, "LB"] = BendersAlg.lower_bounds
+    df[!, "Time"] = BendersAlg.time_iterations
+    CSV.write((@__DIR__)*"/results/benders_larger_root_problem_run$i.csv", df)
 end
